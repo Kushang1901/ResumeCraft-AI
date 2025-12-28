7abe98fb6a3bf74c56f91b28551d6f18dfe1e3d7
Kushang Acharya <kushangacharya8830@gmail.com>
2025-12-28 16:35:18 +0530

    Add TemplateSelection route to App component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import TemplateSelection from "./pages/TemplateSelection";
 import ResumeBuilder from "./pages/ResumeBuilder";
 import Preview from "./pages/preview";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
-
-
-
-
-
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/builder" element={<ResumeBuilder />} />
-                
 
+                {/* HOME */}
+                <Route path="/" element={<HomePage />} />
+
+                {/* TEMPLATE SELECTION (NEW) */}
+                <Route path="/templates" element={<TemplateSelection />} />
+
+                {/* RESUME FLOW */}
+                <Route path="/builder" element={<ResumeBuilder />} />
                 <Route path="/preview" element={<Preview />} />
+
+                {/* AUTH */}
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
-                
-
 
             </Routes>
-
         </BrowserRouter>
     );
 }
