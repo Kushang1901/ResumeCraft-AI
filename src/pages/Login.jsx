a83bfc9795fc96b83340c9f94f95a0c48d4debd1
Kushang Acharya <kushangacharya8830@gmail.com>
2026-01-02 18:25:37 +0530

    Add files via upload

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,8 @@ export default function Login() {
 
             const result = await signInWithPopup(auth, googleProvider);
 
-            await fetch(`${process.env.REACT_APP_API_URL}/signup`, {
+            await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
